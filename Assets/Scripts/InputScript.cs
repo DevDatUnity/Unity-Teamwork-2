1f352b18d1a005d58cb4acd0a066618aaca1e607
krastoss <servicemail945@gmail.com>
2014-12-10 11:24:45 +0200

    revert

--- Assets/Scripts/InputScript.cs
+++ Assets/Scripts/InputScript.cs
@@ -2,16 +2,32 @@
 using System.Collections;
 
 public class InputScript : MonoBehaviour {
-
-    public float moveSpeed = 15;
-    private Vector3 moveDir;
-
-	void Update () {
-        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    public GameObject thePlanet;
+    public float movementSpeed = 0.5f;
+    private bool rotatingForward = false, rotatingLeft = false, rotatingRight = false;
+	void Start () {
+	
 	}
-
-    void FixedUpdate()
-    {
-        rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
-    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+            rotatingForward = true;
+        if (Input.GetKeyDown(KeyCode.D))
+            rotatingRight = true;
+        else if (Input.GetKeyDown(KeyCode.A))
+            rotatingLeft = true;
+        if (Input.GetKeyUp(KeyCode.W))
+            rotatingForward = false;
+        if (Input.GetKeyUp(KeyCode.D))
+            rotatingRight = false;
+        if (Input.GetKeyUp(KeyCode.A))
+            rotatingLeft = false;
+        if(rotatingLeft == true)
+            thePlanet.transform.Rotate(Vector3.left * movementSpeed);
+        if (rotatingRight == true)
+            thePlanet.transform.Rotate(Vector3.right * movementSpeed);
+        if (rotatingForward == true)
+            thePlanet.transform.Rotate(Vector3.forward * movementSpeed);
+	}
 }
