fb038caf906208eefc355e9a09eab8fc20719fa4
krastoss <servicemail945@gmail.com>
2014-12-10 12:09:40 +0200

    mama ti mrasna :dancer:

---
+++ Assets/Scripts/EnemyScripts/EnemyDynamic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDynamic : EnemyMain
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateRadius();
+        if(activated == true)
+        {
+            transform.LookAt(thePlayer.transform, Vector3.left);
+            transform.Translate(Vector3.forward * 0.05f);
+        }
+	}
+}
